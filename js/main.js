ee4d259f28d4f54f76786e597faee7b8338451a0
backyardstudiosa-art <backyardstudiosa@gmail.com>
2026-03-31 07:30:28 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const yearEl = document.getElementById('year');
-  if (yearEl) {
-    yearEl.textContent = new Date().getFullYear().toString();
-  }
-
   if (window.AOS) {
     AOS.init({
       duration: 1000,
